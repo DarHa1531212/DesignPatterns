e8a0dd1217a24b35d7b01208d4ad0ba9024660a4
DarHa1531212 <hans.belanger@pm.me>
2019-04-04 15:03:18 -0400

    started working on 2nd pattern

---
+++ travail2/src/vehicule/Avion.java
@@ -0,0 +1,12 @@
+package vehicule;
+
+public class Avion implements Vehicule {
+    private int passagerMax;
+    private double vitesseCroisiereMax;
+    private int distanceFranchissable;
+
+    public int Avion(int passagerMax, int distanceFranchissable, double vitesseCroisiereMax)
+    {}
+    public void afficherCaracteristiques()
+    {}
+}
